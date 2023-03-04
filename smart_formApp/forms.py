3ce501fda9575977c3a7ea2ee2739f2e0d1f60e0
Giovanni Tshibangu <61130107+Moodkiller20@users.noreply.github.com>
2023-03-04 14:13:48 +0000

    modified:   db.sqlite3 	modified:   smart_formApp/forms.py 	modified:   smart_formApp/urls.py 	modified:   templates/smartform/form.html 	modified:   templates/smartform/user_form.html

--- smart_formApp/forms.py
+++ smart_formApp/forms.py
@@ -6,3 +6,25 @@ class UserForm(forms.ModelForm):
     class Meta:
         model = User
         fields = ['first_name', 'last_name', 'email']
+
+        
+        labels = {
+            'first_name': '',
+            'last_name': '',
+            'emai': '',
+            
+        }
+
+        widgets = {
+
+        'first_name': forms.TextInput(
+            attrs={'class': "form-control", 'placeholder': 'First Name', 'id': 'first-name'}),
+
+        'last_name': forms.TextInput(
+            attrs={'class': "form-control", 'placeholder': 'Last Name', 'id': 'last-name'}),
+        
+        'email': forms.TextInput(
+            attrs={'class': "form-control", 'placeholder': 'Email Address', 'id': 'email'}),
+        
+        
+    }
