b3a73293add00a24f8ca997c450011b529618279
Giovanni Tshibangu <61130107+Moodkiller20@users.noreply.github.com>
2023-03-08 12:00:32 -0500

    update

--- smart_emailApp/views.py
+++ smart_emailApp/views.py
@@ -1,6 +1,23 @@
+from datetime import datetime, timedelta
+
 from django.shortcuts import render
+
+from smart_formApp.models import User
+
 
 # Create your views here.
 
 def home_view(request):
-    return render(request, "smartemail/master_home.html")
+    members_count = User.objects.count()
+    today = datetime.today()
+    thirty_days_ago = today - timedelta(days=7)
+    last7days = User.objects.filter(created_at__gte=thirty_days_ago).count()
+
+
+    context ={
+        "members_count": members_count,
+        "last7days":last7days
+
+    }
+
+    return render(request, "smartemail/master_home.html", context)
