6e898eff95faecbf2e1c36a50ff0a3539774b459
Giovanni Tshibangu <61130107+Moodkiller20@users.noreply.github.com>
2023-03-12 15:41:00 +0000

    #new file:   SmartForm/db.sqlite3 	modified:   db.sqlite3 	modified:   requirements.txt 	modified:   smart_emailApp/tasks.py 	modified:   smart_emailApp/urls.py 	new file:   templates/email_templates/holiday_special.html 	modified:   templates/nav.html

--- smart_emailApp/tasks.py
+++ smart_emailApp/tasks.py
@@ -3,16 +3,14 @@ from tabulate import tabulate
 
 
 
-
 def db_connect():
     # Connect to the database
-    db_path = r'C:\Users\giova\OneDrive\Bureau\Program File\SmartForm\db.sqlite3'
+    db_path = r'./db.sqlite3'
     global conn
     conn = sqlite3.connect(db_path)
     # Create a cursor
     global cursor
     cursor = conn.cursor()
-
 
 
 def executeSelect(query):
@@ -24,13 +22,21 @@ def close_connection():
     cursor.close()
     conn.close()
 
-
-
-
 def printFormat(results):
     
     for result in results:
         print(result)
 
+
+def checkForTask():
+    # checks for task to run today and scheduled . 
+    print(" # Scheduled Task to Run during this period")
+    query =" select task_name, id from smart_emailApp_emailtask Where date_from  BETWEEN date('1004-01-01') AND date('2023-12-31') AND status='Scheduled'; "
+    executeSelect(query)
+
+
+
+
 db_connect()
-print(executeSelect("SELECT * FROM smart_emailApp_emails"))
+# print(executeSelect("SELECT * FROM smart_emailApp_emails"))
+checkForTask()
--- smart_emailApp/urls.py
+++ smart_emailApp/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
                   path('', views.home_view, name='master_home'),
-                  path('scheduled_email', views.scheduled_email, name='scheduled_email'),
+                  #path('scheduled_email', views.scheduled_email, name='scheduled_email'),
                   path('view_emails', views.email_view, name='view_emails'),
                   path('create_email', views.create_email, name='create_email'),
                   path('create_task', views.create_task, name='create_task'),
