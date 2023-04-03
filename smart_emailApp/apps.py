d50babfeb44054a0b2030118704fd1fb4240513d
Giovanni Tshibangu <61130107+Moodkiller20@users.noreply.github.com>
2023-04-03 02:53:29 +0000

    modified:   db.sqlite3 	deleted:    scheduler/__pycache__/__init__.cpython-310.pyc 	deleted:    scheduler/__pycache__/scheduler.cpython-310.pyc 	deleted:    scheduler/__pycache__/send_email.cpython-310.pyc 	deleted:    scheduler/__pycache__/tasks_scheduler.cpython-310.pyc 	modified:   scheduler/tasks_scheduler.py 	modified:   smart_emailApp/apps.py 	modified:   templates/smartemail/master_home.html

--- smart_emailApp/apps.py
+++ smart_emailApp/apps.py
@@ -1,8 +1,6 @@
 from django.apps import AppConfig
 
 
-
-
 class SmartEmailappConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "smart_emailApp"
@@ -10,16 +8,14 @@ class SmartEmailappConfig(AppConfig):
     def ready(self):
         from apscheduler.schedulers.background import BackgroundScheduler
         from django_apscheduler.jobstores import DjangoJobStore, register_events
-        from scheduler.scheduler import Start
+        from scheduler.tasks_scheduler import check_for_task
+        scheduler = BackgroundScheduler()
+        scheduler.add_jobstore(DjangoJobStore(), "default")
+        scheduler.add_job(check_for_task, 'interval', seconds=60, name="Check_for_tasks")
+        register_events(scheduler)
+        scheduler.start()
 
-        from scheduler.tasks_scheduler import check_for_task, update_model
-        # scheduler = BackgroundScheduler()
-        # scheduler.add_jobstore(DjangoJobStore(), "default")
-        # scheduler.add_job(check_for_task, 'interval', seconds=50, name="Check_for_tasks")
-        # register_events(scheduler)
-        # scheduler.start()
 
-        check_for_task()
         #tart()
         #update_model(job)
 
